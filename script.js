3514f2b565c40262844b9b15ac0c17ca975fee6a
Daniel Biasoli <108940773+danielbiasoli@users.noreply.github.com>
2024-06-05 09:44:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = documentquerySelector (".caixa-principal")
+const caixaPerguntass = documentquerySelector (".caixa-perguntas")
+const caixaAlternativas = documentquerySelector (".caixa-alternativas")
+const caixaResultado = documentquerySelector (".caixa-resultado")
+const caixaResultado = documentquerySelector (".texto-resultado")
+
+const perguntas = [
+{
+enunciado: "Pergunta1"
+Alternativas: ["Alternativa1", "Alternativa2"]
+},
+{
+enunciado: "Pergunta2"
+Alternativas: ["Alternativa1", "Alternativa2"]
+ },
+]
